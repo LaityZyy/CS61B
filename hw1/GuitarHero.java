1ce21ad10b8f68fda3afabd4d0a56a02e201194a
unknown <3216100019@qq.com>
2023-07-15 13:08:10 +0800

    completed hw1

--- hw1/GuitarHero.java
+++ hw1/GuitarHero.java
@@ -5,7 +5,7 @@ public class GuitarHero {
     private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
     private static final int totalNotes = 37;
 
-    private static double ithConcert(int i){
+    private static double ithConcert(int i) {
         return CONCERT_A * Math.pow(2, (i - 24.0) / 12);
     }
     public static void main(String[] args) {
@@ -14,22 +14,22 @@ public class GuitarHero {
             gs[i] = new synthesizer.GuitarString(ithConcert(i));
         }
 
-        while(true){
-            if (StdDraw.hasNextKeyTyped()){
+        while(true) {
+            if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
                 int index = keyboard.indexOf(key);
-                if (index < 0){
+                if (index < 0) {
                     System.out.println("please enter valid key : " + keyboard);
                     continue;
                 }
                 gs[index].pluck();
             }
             double sample = 0;
-            for (synthesizer.GuitarString string : gs){
+            for (synthesizer.GuitarString string : gs) {
                 sample += string.sample();
             }
             StdAudio.play(sample);
-            for (synthesizer.GuitarString string : gs){
+            for (synthesizer.GuitarString string : gs) {
                 string.tic();
             }
         }
