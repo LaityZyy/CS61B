7d0a5d629f10d9f6f9a4d174b7ceff5cf7a7f6d6
unknown <3216100019@qq.com>
2023-07-21 14:11:30 +0800

    completed hw4

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,10 +1,127 @@
 package hw4.puzzle;
 
-public class Board {
+import edu.princeton.cs.algs4.Queue;
+
+public class Board implements WorldState {
+    private final int[][] tiles;
+    private static final int BLANK = 0;
+    private int N;
+    private int estimatedDist;
+    public Board(int[][] tiles) {
+        if (tiles == null || tiles[0] == null || tiles.length != tiles[0].length) {
+            throw new IllegalArgumentException();
+        }
+        N = tiles.length;
+        estimatedDist = -1;
+        this.tiles = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                this.tiles[i][j] = tiles[i][j];
+            }
+        }
+    }
+    public int tileAt(int i, int j) {
+        if (i < 0 || j < 0 || i >= N || j >= N) {
+            throw new IndexOutOfBoundsException();
+        }
+        return tiles[i][j];
+    }
+    public int size() {
+        return N;
+    }
+    public Iterable<WorldState> neighbors() {
+        Queue<WorldState> neighbors = new Queue<>();
+        int hug = size();
+        int bug = -1;
+        int zug = -1;
+        for (int rug = 0; rug < hug; rug++) {
+            for (int tug = 0; tug < hug; tug++) {
+                if (tileAt(rug, tug) == BLANK) {
+                    bug = rug;
+                    zug = tug;
+                }
+            }
+        }
+        int[][] ili1li1 = new int[hug][hug];
+        for (int pug = 0; pug < hug; pug++) {
+            for (int yug = 0; yug < hug; yug++) {
+                ili1li1[pug][yug] = tileAt(pug, yug);
+            }
+        }
+        for (int l11il = 0; l11il < hug; l11il++) {
+            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
+                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
+                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
+                    ili1li1[l11il][lil1il1] = BLANK;
+                    Board neighbor = new Board(ili1li1);
+                    neighbors.enqueue(neighbor);
+                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
+                    ili1li1[bug][zug] = BLANK;
+                }
+            }
+        }
+        return neighbors;
+    }
+    public int hamming() {
+        int dist = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tiles[i][j] == 0) {
+                    continue;
+                } else if (tiles[i][j] != i * N + j + 1) {
+                    dist++;
+                }
+            }
+        }
+        return dist;
+    }
+    public int manhattan() {
+        int dist = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tiles[i][j] == 0) {
+                    continue;
+                } else {
+                    int targeti = (tiles[i][j] - 1) / N;
+                    int targetj = (tiles[i][j] - 1) % N;
+                    dist += Math.abs(targeti - i);
+                    dist += Math.abs(targetj - j);
+                }
+            }
+        }
+        return dist;
+    }
+    public int estimatedDistanceToGoal() {
+        if (estimatedDist == -1) {
+            estimatedDist = manhattan();
+        }
+        return estimatedDist;
+    }
+    public boolean equals(Object y) {
+        if (y == this) return true;
+        if (y == null || this.getClass() != y.getClass()) {
+            return false;
+        }
+        Board other = (Board) y;
+        if (this.N != other.N) return false;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (this.tiles[i][j] != other.tiles[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 
     /** Returns the string representation of the board. 
       * Uncomment this method. */
-    /*public String toString() {
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
@@ -16,6 +133,6 @@ public class Board {
         }
         s.append("\n");
         return s.toString();
-    }*/
+    }
 
 }
